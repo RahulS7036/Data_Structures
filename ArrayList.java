0937120d826014d2c1bd015309f0ce274dd5964e
Sabale_Rahul <sabaler94@gmail.com>
2023-07-21 09:54:17 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,20 @@
+//Sorting elements of ArrayList
+
+import java.util.*;
+class new12 {
+
+    public static void main(String args[]){
+            System.out.println("Hello Rahul");
+      ArrayList<Integer> arr = new ArrayList<Integer>(5);
+       arr.add(20);
+       arr.add(30);
+       arr.add(10);
+       
+      Collections.sort(arr);
+       System.out.print(arr);
+
+
+
+    }
+    
+}
