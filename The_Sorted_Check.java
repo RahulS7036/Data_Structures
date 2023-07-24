2a40469c0999ba67e038d4514da30060c9435b9f
Sabale_Rahul <sabaler94@gmail.com>
2023-07-24 16:05:57 +0530

    Create The_Sorted_Check.java

---
+++ The_Sorted_Check.java
@@ -0,0 +1,17 @@
+public class Solution {
+   public static int isSorted(int n, int []a) {
+        // Write your code here.
+        int flag = 1;
+       for(int i = 0;i < n;i++){
+           if(i == n - 1){
+               break;
+           }
+           else if(a[i] > a[i+1]){
+               flag = 0;
+               break;
+           }
+       }
+       return flag;
+
+    }
+}
